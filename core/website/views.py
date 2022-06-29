aabf594cfddc5bf2431a53f0b80fde022d92177a
DevPrinceK <princesamuelpks@gmail.com>
2022-06-29 18:51:13 +0000

    update

--- core/website/views.py
+++ core/website/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class IndexView(View):
+    template = 'website/index.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, self.template, context)
