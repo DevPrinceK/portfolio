dd0deaf617fc51980e2df642a786ae4c07a1d536
DevPrinceK <princesamuelpks@gmail.com>
2022-06-29 22:44:46 +0000

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -77,6 +77,13 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
+MEDIA_ROOT = BASE_DIR / 'assets/'
+MEDIA_URL = '/assets/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
