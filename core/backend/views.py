c82d42e1b70cce5b5edfe0ff1290dea22cd3bf4e
DevPrinceK <princesamuelpks@gmail.com>
2022-06-30 22:32:38 +0000

    Update views.py

--- core/backend/views.py
+++ core/backend/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 
+from backend.models import Blog
+
 
 class BlogsView(View):
     template = 'backend/blogs.html'
 
     def get(self, request, *args, **kwargs):
-        context = {}
+        context = {
+            'blogs': Blog.objects.all().order_by('-id')
+        }
         return render(request, self.template, context)
