acba83e2bf957793bba056c79e1f6ca749f94e1e
DevPrinceK <princesamuelpks@gmail.com>
2022-06-30 22:33:41 +0000

    Update views.py

--- core/website/views.py
+++ core/website/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 
+from backend.models import Blog
+
 
 class IndexView(View):
     template = 'website/index.html'
 
     def get(self, request, *args, **kwargs):
-        context = {}
+        blogs = Blog.objects.all()
+        context = {
+            'blogs': blogs,
+        }
         return render(request, self.template, context)
 
 
@@ -14,5 +19,13 @@ class BlogView(View):
     template = 'website/blog.html'
 
     def get(self, request, *args, **kwargs):
-        context = {}
+        blog_id = request.GET.get('blog_id')
+        blog = Blog.objects.filter(id=blog_id).first()
+
+        tags = blog.tags.all()
+
+        context = {
+            'blog': blog,
+            'tags': tags,
+        }
         return render(request, self.template, context)
