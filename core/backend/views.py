c9396aa4356f01a30040caaf544b2357cdb7863d
DevPrinceK <princesamuelpks@gmail.com>
2022-06-30 00:27:03 +0000

    Update views.py

--- core/backend/views.py
+++ core/backend/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class BlogsView(View):
+    template = 'backend/blogs.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, self.template, context)
