d9c93cc0f8d9917ef085f21a5e83f0eb3046dce9
DevPrinceK <princesamuelpks@gmail.com>
2022-07-11 12:52:37 +0000

    Update admin.py

--- core/backend/admin.py
+++ core/backend/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from .models import Blog, Category, Tag, Work, Client
+from .models import Blog, Category, Work, Client
 
 admin.site.register(Blog)
 admin.site.register(Category)
-admin.site.register(Tag)
 admin.site.register(Work)
 admin.site.register(Client)
