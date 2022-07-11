c781adb8fa9a1ec3d91f43292199f50e44a71794
DevPrinceK <princesamuelpks@gmail.com>
2022-07-11 12:54:28 +0000

    Update urls.py

--- core/backend/urls.py
+++ core/backend/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'backend'
 urlpatterns = [
     path('', views.BlogsView.as_view(), name='blogs'),
+    path('create-update-blog/', views.CreateUpdateBlogView.as_view(), name='create_update_blog'),  # noqa
+    path('delete-blog/', views.DeleteBlogPostView.as_view(), name='delete_blog'),
+
 ]
