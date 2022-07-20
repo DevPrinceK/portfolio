4e2c8d1be99495041d9d51c0c62de05aeda4c55b
DevPrinceK <princesamuelpks@gmail.com>
2022-07-20 20:24:37 +0000

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -101,3 +101,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # EXTERNAL API CONFIG
 ARKESEL_API_KEY = os.getenv('ARKESEL_API_KEY')
+
+# Email backend with google mail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
